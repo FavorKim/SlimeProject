ee35ccdb2d1616e92dc541c577558aa3f6408562
김하랑 <kimharangs@gmail.com>
2024-07-11 18:54:57 +0900

    [init] 오브젝트 에셋 추가 및 프리팹 생성 시 에셋 추가
    프리팹 생성 시, 해당 오브젝트의 datainfo와 동일한 이름을 가진 에셋을 프리팹의 자식으로 설정하고 생성

--- Assets/Favor/Scripts/Object/ObjectBase.cs
+++ Assets/Favor/Scripts/Object/ObjectBase.cs
@@ -7,15 +7,36 @@ public enum ObjectType
 
 public class ObjectBase : MonoBehaviour
 {
+    [Space(20)]
+    
+    [Tooltip("데이터 정보")]
     public string datainfo;
+    [Tooltip("오브젝트 이름")]
     public string description;
+    
+    [Space(20)]
+
     public int ID;
+    [Tooltip("사용제한횟수")]
     public int usecount;
+    [Tooltip("내구도")]
     public int durabillity;
+    [Tooltip("밟기 상호작용을 일으키는 무게 최소값")]
     public int masslimit;
+    [Tooltip("물체의 질량(무게)")]
     public int mass;
+    
+    [Space(20)]
+
+    [Tooltip("들수 있음의 여부")]
     public bool holdable;
+    [Tooltip("충돌하는지의 여부")]
     public bool collide;
+    [Tooltip("소멸가능한지의 여부")]
     public bool deletable;
+    
+    [Space(20)]
+
+    [Tooltip("오브젝트의 타입")]
     public ObjectType type;
 }
--- Assets/Favor/Scripts/Util/PrefabCreator.cs
+++ Assets/Favor/Scripts/Util/PrefabCreator.cs
@@ -7,8 +7,18 @@ public class PrefabCreator
     {
         string normalSaveRoot = $"Assets/Favor/Prefabs/Normal/{prefab.datainfo}.prefab";
         string attackSaveRoot = $"Assets/Favor/Prefabs/Attack/{prefab.datainfo}.prefab";
-
+        var resource = Resources.Load<GameObject>($"Favor/OutLook/{prefab.datainfo}");
+        if (resource != null)
+        {
+            GameObject outLook = GameObject.Instantiate(resource, prefab.gameObject.transform);
+            if (outLook != null)
+            {
+                outLook.transform.localRotation = Quaternion.identity;
+                outLook.transform.localPosition = Vector3.zero;
+            }
+        }
         string root = prefab is AttackObjectBase ? attackSaveRoot : normalSaveRoot;
+        
         Object obj = PrefabUtility.SaveAsPrefabAsset(prefab.gameObject, root);
 
         if (obj != null)

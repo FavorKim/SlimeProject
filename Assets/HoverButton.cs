4531ed9073c7fc64d110adfc4dd1e2adf56020e0
최강건 <gongun99@gmail.com>
2024-07-12 18:07:47 +0900

    [Update] 마우스가 스테이지 이미지 위에 올라올때 크기가 커짐

--- Assets/HoverButton.cs
+++ Assets/HoverButton.cs
@@ -4,21 +4,53 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class HoverButton : MonoBehaviour, IPointerEnterHandler , IPointerExitHandler
+public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Button button;
+    public RectTransform imageRectTransform; // UI 이미지의 RectTransform
+    public RectTransform selectLineRectTransform;
+    private Vector2 imgOriginalSize = new Vector2(160, 200);
+    private Vector2 selectLineOriginalSize = new Vector2(170, 210);
+    public Vector2 hoverSize = new Vector2(1.1f, 1.1f);
+
     private void Start()
     {
         button.gameObject.SetActive(false);
+
+        // 초기 크기 설정 (필요시)
+        if (imageRectTransform != null)
+        {
+            imgOriginalSize = imageRectTransform.sizeDelta;
+        }
+        if (selectLineRectTransform != null)
+        {
+            selectLineOriginalSize = selectLineRectTransform.sizeDelta;
+        }
     }
-    
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         button.gameObject.SetActive(true);
+        if (imageRectTransform != null && selectLineRectTransform != null)
+        {
+            selectLineRectTransform.gameObject.SetActive(true);
+
+            // 크기 변경 (원래 크기에서 비율을 곱하여 조정)
+            imageRectTransform.sizeDelta = new Vector2(imgOriginalSize.x * hoverSize.x, imgOriginalSize.y * hoverSize.y);
+            selectLineRectTransform.sizeDelta = new Vector2(selectLineOriginalSize.x * hoverSize.x, selectLineOriginalSize.y * hoverSize.y);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         button.gameObject.SetActive(false);
+        if (imageRectTransform != null && selectLineRectTransform != null)
+        {
+            selectLineRectTransform.gameObject.SetActive(false);
+
+            // 원래 크기로 되돌림
+            imageRectTransform.sizeDelta = imgOriginalSize;
+            selectLineRectTransform.sizeDelta = selectLineOriginalSize;
+        }
     }
 }

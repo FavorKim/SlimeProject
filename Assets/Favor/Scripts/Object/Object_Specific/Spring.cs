ae38795ae1ddeb8e8260e44172df8b8d605ce387
김하랑 <kimharangs@gmail.com>
2024-07-17 17:43:53 +0900

    [feat] 위아래로 돌아가는 문 (door_slide) 추가

--- Assets/Favor/Scripts/Object/Object_Specific/Spring.cs
+++ Assets/Favor/Scripts/Object/Object_Specific/Spring.cs
@@ -14,17 +14,16 @@ public class Spring : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.TryGetComponent(out Rigidbody rb))
+        if (other.gameObject.TryGetComponent(out Rigidbody rb))
         {
-            Debug.Log(other.gameObject.name + "obj");
             rb.velocity = Vector3.zero;
             rb.AddForce(Vector3.up * springPow);
         }
-        else if(other.gameObject.transform.root.TryGetComponent(out Rigidbody rbParent))
+        else if (other.gameObject.transform.root.TryGetComponent(out Rigidbody rbParent))
         {
-            Debug.Log(other.gameObject.name + "root");
             rbParent.velocity = Vector3.zero;
             rbParent.AddForce(Vector3.up * springPow);
         }
     }
+
 }
